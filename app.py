6e2c1f92798c84d2f7d8abc8ce0547e504a62da1
Jorge Miguel Sanchez <jmsanchez@axesor.com>
2021-07-14 12:08:41 +0200

    Test Azure app

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/GetRiim/<int:entityId>")
+def get_riim(entityId):
+    return "Hello, World!" + entityId
