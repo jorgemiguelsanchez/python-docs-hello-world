323f41742ec8815a5a3c224d39d8f2356b88c5fd
Jorge Miguel Sanchez <jmsanchez@axesor.com>
2021-07-15 10:52:24 +0200

    test

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import io
 import codecs
 
-from flask import Flask
+from flask import Flask, send_file
+from random import seed
+from random import randint
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,12 +13,22 @@ def hello():
 
 @app.route("/GetRiim/<int:entityId>")
 def get_riim(entityId):
-    inputfile = "./resources/1.png"
-    f=codecs.open(inputfile, 'r')
-    image = f.read()
-    response = Flask.make_response(image)
-    response.headers.set('Content-Type', 'image/png')
+    seed(entityId)
+    riim = randint(1,5)
+
+    inputfile = "./resources/%s.png" % str(riim)
+    image = bytearray()    
+    f=open(inputfile, 'rb')
+    
+    byte = f.read(1)    
+    while byte:
+        image.append(byte[0])
+        byte =f.read(1)
+   
+    f.close()
+    #response = Flask.make_response(image)
+    #response.headers.set('Content-Type', 'image/png')
     #response.headers.set('Content-Disposition', 'attachment', filename='%s.jpg' % pid)
-    response.headers.set('Content-Disposition', 'attachment', filename=inputfile)
-    #return "Entity Value:" + str(entityId)
-    return response
+    #response.headers.set('Content-Disposition', 'attachment', filename=inputfile)
+    #return "Entity Value:" + str(entityId)    
+    return send_file(io.BytesIO(image) ,attachment_filename='logo.png',mimetype='image/png')
